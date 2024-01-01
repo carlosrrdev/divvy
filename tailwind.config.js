daf0c8d1af941abe8a4da664fc6afd00dd906b89
carlosrrdev <rr.carlos.dev@gmail.com>
2024-01-01 13:05:58 -0600

    Add desktop styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,25 @@ export default {
     extend: {
       colors: {
         "indigo-1000": "#01030A"
+      },
+      animation: {
+        "enter-right": "enterRight 1s ease-in-out forwards",
+        "enter-left": "enterLeft 1s ease-in-out forwards",
+        "fade-in": "fadeIn 1s ease-in-out forwards",
+      },
+      keyframes: {
+        enterRight: {
+          "0%": {opacity: 0, transform: "translateX(-10px)"},
+          "100%": {opacity: 1, transform: "translateX(0)"}
+        },
+        enterLeft: {
+          "0%": {opacity: 0, transform: "translateX(10px)"},
+          "100%": {opacity: 1, transform: "translateX(0)"}
+        },
+        fadeIn: {
+          "0%": {opacity: 0},
+          "100%": {opacity: 1}
+        }
       }
     },
   },
