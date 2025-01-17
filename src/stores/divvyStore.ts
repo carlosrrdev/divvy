c7822996dfcdad46b7255939c98a16544447eb05
carlosrrdev <rr.carlos.dev@gmail.com>
2025-01-17 10:29:52 -0600

    Begin logic for adding members

--- src/stores/divvyStore.ts
+++ src/stores/divvyStore.ts
@@ -18,6 +18,11 @@ interface DivvyStore {
   divvyIsValidForDivvyUp: boolean;
   divvyMembers: Member[];
   divvyExpenses: Expense[];
+  addNewMember: (memberName: string) => void;
+  addNewExpense: (expenseName: string, expenseAmount: number) => void;
+  addExpenseToMember: (memberId: string, expenseId: string) => void;
+  removeMember: (memberId: string) => void;
+  removeExpense: (expenseId: string) => void;
 }
 
 export const divvyStore: DivvyStore = {
@@ -28,4 +33,33 @@ export const divvyStore: DivvyStore = {
   divvyIsValidForDivvyUp: false,
   divvyMembers: [],
   divvyExpenses: [],
+  addNewMember(memberName: string) {
+    this.divvyMembers.push({
+      name: memberName,
+      id: crypto.randomUUID(),
+      expenses: [],
+    });
+  },
+  addNewExpense(expenseName: string, expenseAmount: number) {
+    this.divvyExpenses.push({
+      name: expenseName,
+      id: crypto.randomUUID(),
+      amount: expenseAmount,
+    });
+  },
+  addExpenseToMember(memberId: string, expenseId: string) {
+    const member = this.divvyMembers.find((member) => member.id === memberId);
+    if (member) {
+      member.expenses.push(expenseId);
+    }
+  },
+  removeMember(memberId: string) {
+    this.divvyMembers = this.divvyMembers.filter((member) => member.id !== memberId);
+  },
+  removeExpense(expenseId: string) {
+    this.divvyExpenses = this.divvyExpenses.filter((expense) => expense.id !== expenseId);
+    this.divvyMembers.forEach((member) => {
+      member.expenses = member.expenses.filter((expenseId) => expenseId !== expenseId);
+    });
+  },
 }
