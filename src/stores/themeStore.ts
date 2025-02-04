bbd92e41714da7160c0a0bb8b4e363d93c8f3f04
carlosrrdev <rr.carlos.dev@gmail.com>
2025-02-04 08:15:19 -0600

    Add missing files

---
+++ src/stores/themeStore.ts
@@ -0,0 +1,30 @@
+type theme = "light" | "dark" | "system";
+
+export const themeStore = {
+  theme: "system" as theme,
+  showDropdown: false,
+  init() {
+    this.theme = (localStorage.getItem("theme") as theme) || "system";
+    this.setTheme(this.theme);
+  },
+  setTheme(theme: theme) {
+    const root = document.documentElement;
+    root.classList.remove("light", "dark");
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
+      root.classList.add(systemTheme);
+      localStorage.removeItem("theme");
+      this.showDropdown = false;
+      return;
+    }
+    localStorage.setItem("theme", theme);
+    root.classList.add(theme);
+    this.showDropdown = false;
+  },
+  toggleDropdown(show = false) {
+    this.showDropdown = show;
+  }
+};
